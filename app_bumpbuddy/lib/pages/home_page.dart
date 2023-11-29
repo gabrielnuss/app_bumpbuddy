4b36833f5ee4efe53d03d167118210f987ba93b2
Gabriel Nuss <gabrielnuss40@gmail.com>
2023-11-28 22:37:39 -0300

    adicionado telas lista de semanas

--- app_bumpbuddy/lib/pages/home_page.dart
+++ app_bumpbuddy/lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:app_bumpbuddy/pages/calendario_page.dart';
+import 'package:app_bumpbuddy/pages/dicas_page.dart';
+import 'package:app_bumpbuddy/pages/semanas_page.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,14 +11,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  PageController pageController = PageController();
+  int posicaoPagina = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Home Page"),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          leading: Icon(
+            Icons.person_2_outlined,
+            color: Theme.of(context).colorScheme.inverseSurface,
+          ),
+          backgroundColor: Theme.of(context).colorScheme.background,
+          title: Text(
+            "Home Page",
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.inverseSurface,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: PageView(
+          controller: pageController,
+          onPageChanged: (value) {
+            setState(() {
+              posicaoPagina = value;
+            });
+          },
+          children: const [CalendarioPage(), SemanasPage(), DicasPage()],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Theme.of(context).colorScheme.inverseSurface,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            showUnselectedLabels: false,
+            onTap: (value) {
+              setState(() {
+                pageController.jumpToPage(value);
+              });
+            },
+            currentIndex: posicaoPagina,
+            items: const [
+              BottomNavigationBarItem(
+                  label: "Calendário", icon: Icon(Icons.calendar_month)),
+              BottomNavigationBarItem(
+                  label: "Semanas", icon: Icon(Icons.stroller)),
+              BottomNavigationBarItem(label: "Dicas", icon: Icon(Icons.info)),
+            ]),
       ),
-      body: const Center(child: Text("Home page")),
     );
   }
 }
---
+++ app_bumpbuddy/lib/pages/semanas_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SemanasPage extends StatefulWidget {
+  const SemanasPage({super.key});
+
+  @override
+  State<SemanasPage> createState() => _SemanasPageState();
+}
+
+class _SemanasPageState extends State<SemanasPage> {
+  ScrollController _scrollController = ScrollController();
+
+  List<String> semanas = [
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+    "lib/assets/images/bump_buddy.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (ScrollNotification scrollInfo) {
+        setState(() {}); // Rebuild the widget on scroll to update image scales
+        return true;
+      },
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: semanas.length,
+        itemBuilder: (BuildContext context, int index) {
+          var semana = semanas[index];
+          double scale = 1.0;
+          double position = index * 100.0 - _scrollController.offset;
+          if (position < 0) {
+            scale = 1.0 +
+                position / 500; // Adjust the divisor for desired scaling effect
+          } else {
+            scale = 1.0 - position / 500;
+          }
+          scale = scale.clamp(0.5, 1.0); // Limit the minimum and maximum scale
+          return ParallaxImage(
+            scale: scale,
+            semana: semana,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ParallaxImage extends StatelessWidget {
+  final double scale;
+
+  final String semana;
+
+  ParallaxImage({required this.scale, required this.semana});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Transform.scale(
+        scale: scale,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(300),
+          child: Image.asset(
+            semana,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
